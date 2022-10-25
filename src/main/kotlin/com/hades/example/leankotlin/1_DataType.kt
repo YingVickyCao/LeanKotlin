22f3af41d573e01559becf6ecec429846d031a48
Vicky <ying.vicky.cao@outlook.com>
2022-10-25 22:32:51 +0800

    Update 1_DataType.kt

--- src/main/kotlin/com/hades/example/leankotlin/1_DataType.kt
+++ src/main/kotlin/com/hades/example/leankotlin/1_DataType.kt
@@ -28,13 +28,13 @@ fun main() {
     var age2 = 18
 
     // 3 空安全类型
-    // (1) non-null type 不能赋值 null
-    var nameOfNon_null: String = "Vicky"      // non-null
+    // (1) non-null type,  不能赋值 null
+    var nameOfNon_null: String = "Vicky"
     // compile error : Null can not be a value of a non-null type String
 //    val nameOfNon_null: String = null
 
-    // (2) nullable type
-    var nameOfNullable: String? = null       // nullable (null / not null)
+    // (2) nullable type (null / not null)
+    var nameOfNullable: String? = null
 
     // (3) non-null type  cannot= nullable type, but can use !! to force convert nullable type to non-null type
     /**
