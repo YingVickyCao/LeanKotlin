4ebb05997de932b6fc25c1d74eef0fc2a2a87992
yingvickycao <ying.vicky.cao@outlook.com>
2023-11-23 22:25:36 +0800

    function

--- src/main/kotlin/com/hades/example/leankotlin/_5_functions/_1_functions/FunctionScopeExample.kt
+++ src/main/kotlin/com/hades/example/leankotlin/_5_functions/_1_functions/FunctionScopeExample.kt
@@ -1,37 +1,105 @@
 package com.hades.example.leankotlin._5_functions._1_functions
 
 // https://kotlinlang.org/docs/functions.html#function-scope
+// https://kotlinlang.org/docs/functions.html#function-scope
 fun main() {
-}
+    /**
+     * Example 2 :Function scope
+     *
+     */
+    /**
+     * Example 2 :Function scope - Local functions
+     *
+     */
+    run {
+        val result = sum(1, 2, 3)
+        println(result) // 6
+    }
 
-/**
- * Function scope
- * Local functions
- * Member functions
- * Generic functions
- * Tail recursive functions
- */
+
+    /**
+     * Example 2 :Function scope - Member functions
+     *
+     */
+
+    /**
+     * Example 2 :Function scope - Generic functions
+     *
+     */
+    /**
+     * Example 2 :Function scope - Tail recursive functions
+     *
+     */
+
+    println(findFixPoint(5.0))  // 0.7390851332712491
+    println(findFixPoint2(5.0)) // 0.7390851331706995
+}
 
 /**
  * Example 2 :Function scope
  *
  */
-
 /**
  * Example 2 :Function scope - Local functions
  *
  */
+// Local function (局部函数) : 一个函数在另一个函数内部
+// A local function can access local variables of outer functions (the closure). In the case above, visited can be a local variable:
+fun sum(num1: Int, num2: Int, num3: Int): Int {
+    /**
+     * add is local function inside sum function
+     */
+    var visited = false
+    fun add(num1: Int, num: Int): Int {
+        println(visited)
+        return num1 + num
+    }
+
+    var sum = add(num1, num2);
+    visited = true
+    sum = add(sum, num3);
+    return sum
+}
 
 /**
  * Example 2 :Function scope - Member functions
  *
  */
+class Sample {
+    fun foo() { // 成员函数是在类或对象内部定义的函数
+        println("foo")
+    }
+}
 
 /**
  * Example 2 :Function scope - Generic functions
  *
  */
+// 函数可以有泛型参数
+fun <T> singletonList(item: T): List<T> {
+    return emptyList()
+}
+
 /**
  * Example 2 :Function scope - Tail recursive functions
  *
  */
+// 尾递归函数
+// 对于某些使用循环的算法，可以使用尾递归替代而不会有堆栈溢出的风险。 当一个函数用 tailrec 修饰符标记并满足所需的形式条件时，编译器会优化该递归， 留下一个快速而高效的基于循环的版本
+
+// 计算余弦的不动点（fixpoint of cosine）
+// 它只是重复地从 1.0 开始调用 Math.cos， 直到结果不再改变，对于这里指定的 eps 精度会产生 0.7390851332151611 的结果。
+val eps = 1E-10
+tailrec fun findFixPoint(x: Double = 1.0): Double = if (Math.abs(x - Math.cos(x)) < eps) x else findFixPoint(Math.cos(x))
+
+// 相当于传统风格的代码
+private fun findFixPoint2(x: Double = 1.0): Double {
+    var x = 1.0
+    while (true) {
+        val y = Math.cos(x)
+        if (Math.abs(x - y) < eps) return x
+        x = Math.cos(x)
+    }
+}
+// TODO:  tailrec
+// 要符合 tailrec 修饰符的条件的话，函数必须将其自身调用作为它执行的最后一个操作。在递归调用后有更多代码时， 不能使用尾递归，不能用在 try/catch/finally 块中，也不能用于 open 的函数。 目前在 Kotlin for the JVM 与 Kotlin/Native 中支持尾递归。
--- src/main/kotlin/com/hades/example/leankotlin/_5_functions/_1_functions/FunctionUsageExample.kt
+++ src/main/kotlin/com/hades/example/leankotlin/_5_functions/_1_functions/FunctionUsageExample.kt
@@ -1,5 +1,7 @@
 package com.hades.example.leankotlin._5_functions._1_functions
 
+import java.util.ArrayList
+
 // https://kotlinlang.org/docs/functions.html#function-usage
 // https://book.kotlincn.net/text/functions.html
 fun main() {
@@ -51,11 +53,9 @@ fun main() {
 
         // [foo3] bar = 0,baz = 1
         // 使用bar 和 baz 的默认值
-        foo3(
-            qux = {
-                println("hello")
-            }
-        )
+        foo3(qux = {
+            println("hello")
+        })
 
         // [foo3] bar = 0,baz = 1
         // 使用bar 和 baz 的默认值
@@ -68,35 +68,108 @@ fun main() {
      * Example 1 : Function usage - Named arguments
      */
 
-    run { }
+    run {
+        reformat("str2")
+        reformat("str2", false)
+        reformat("str2", false, true)
+        reformat("str2", false, true, false)
+        reformat("str2", false, true, false, '-')
+        reformat("str", false, upperCaseFirstLetter = false, divideByCamelHumps = true, '-')
+        reformat("str", upperCaseFirstLetter = false, wordSeparator = '-')
+
+        foo4(strings = arrayOf("a", "b", "c"))  // [a, b, c]
+    }
     /**
      * Example 1 : Function usage - Unit-returning functions
      */
-    run { }
+    run {
+        printInt(5)
+        printInt2(5)
+
+        val course = "Math"
+        println(printCourse(course)) // true
+    }
 
 
     /**
      * Example 1 : Function usage - Single-expression functions
      */
-    run { }
+    run {
+        println(doubleValue(3)) // 6
+    }
 
     /**
      * Example 1 : Function usage - Explicit return types
      */
 
-    run { }
-
     /**
      * Example 1 : Function usage - Variable number of arguments (varargs)
      */
-    run { }
+    run {
+        // 1  2  3
+        val list: List<Int> = asList(1, 2, 3)
+        for (item in list) {
+            print("$item  ")
+        }
+        println()
+
+        val a = intArrayOf(1, 2, 3)
+        println()
+
+        // -1  1  2  3  4
+        val list2 = asList(-1, *a.toTypedArray(), 4) // 展开操作符（在数组前面加 *）
+        for (item in list2) {
+            print("$item  ")
+        }
+        println()
+    }
 
 
     /**
      * Example 1 : Function usage - Infix notation
      */
 
-    run { }
+    run {
+        // 1 Define an infix function on Int
+        infix fun Int.times(str: String) = str.repeat(this)
+        println(2 times "bye ") // bye bye
+        // is equal to
+        println("bye ".repeat(2))   // bye bye
+
+        // 2 Create a pair using infix function on from standard lib
+        val pair = "A" to "BC"
+        println(pair)           // (A, BC)
+        println(pair.first)     // A
+        println(pair.second)    // BC
+
+        val pair2 = "A" to "BC" to "d"
+        println(pair2)              // ((A, BC), d)
+        println(pair2.first.first)  // A
+        println(pair2.first.second) // BC
+        println(pair2.second)       // d
+
+        // 3 Custom a pair
+        infix fun String.onto(other: String) = Pair(this, other)
+        var myPair = "Hello" onto "World"
+        println(myPair)         // (Hello, World)
+        println(myPair.first)   // Hello
+        println(myPair.second)  // World
+        println(myPair.second.onto("XY")) // (World, XY)
+
+
+        println(10 sh 20)       // 30
+        println(10.sh(20))   // 30  等价
+
+        println(1 sh 2 + 3)     // 6
+        println(1 sh (2 + 3))   // 6  等价
+
+        val n: Int = 2
+        0 until n * 2
+        0 until (n * 2) // 等价
+
+        // TODO: xs union ys as Set<*> 等价于 xs union (ys as Set<*>)
+        // xs union ys as Set<*> 等价于 xs union (ys as Set<*>)
+    }
 
 }
 
@@ -171,20 +244,99 @@ fun foo3(bar: Int = 0, baz: Int = 1, qux: () -> Unit) {
 /**
  * Example 1 : Function usage - Named arguments
  */
+// 调用函数时，指定一个参数或多个参数, 让调用更可读
+fun reformat(str: String, normalizeCase: Boolean = true, upperCaseFirstLetter: Boolean = true, divideByCamelHumps: Boolean = false, wordSeparator: Char = ' ') {
+
+}
+
+// pass a variable number of arguments (vararg) with names using the spread operator:
+fun foo4(vararg strings: String) {
+    println(strings.contentDeepToString())
+}
 
 /**
  * Example 1 : Function usage - Unit-returning functions
  */
 
+// 一个函数不返回有用的值时，其返回值是Unit。Unit返回值类型可以省略
+// This method does not have return
+fun printInt(id: Int) {
+    println(id)
+}
+
+// This method does not have return
+fun printInt2(id: Int): Unit {
+    println(id)
+}
+
+// this method returns bool value
+fun printCourse(course: String): Boolean {
+    //
+    println("Course is $course")
+    return true
+}
+
 /**
  * Example 1 : Function usage - Single-expression functions
  */
+//fun doubleValue(x: Int): Int {
+//    return x * 2
+//}
+
+// 当函数体由单个表达构成时，可省略花括号
+fun doubleValue(x: Int): Int = x * 2
+
 /**
  * Example 1 : Function usage - Explicit return types
  */
+// 具有块代码体的函数必须指定显式返回类型，如果返回值是Unit，可以省略。
+
 /**
  * Example 1 : Function usage - Variable number of arguments (varargs)
  */
+// 函数的参数（通常是最后一个），用可以vararg修饰，表示可变参数
+// 只有一个参数可以标注为 vararg。
+// 如果vararg 参数不是列表中的最后一个参数， 可以使用具名参数语法传递其后的参数的值
+// 如果参数具有函数类型，则通过在括号外部传一个 lambda。
+// 如果已经有一个数组并希望将其内容传给该函数，那么使用展开操作符（在数组前面加 *）
+fun <T> asList(vararg ts: T): List<T> {
+    val result = ArrayList<T>()
+    for (t in ts) {
+        result.add(t)
+    }
+    return result
+}
+
 /**
  * Example 1 : Function usage - Infix notation
  */
+// infix 表示 中缀表示法（忽略该调用的点与圆括号）调用
+// 中缀函数必须满足以下要求：
+// 它们必须是成员函数或扩展函数。
+// 它们必须只有一个参数。
+// 其参数不得接受可变数量的参数且不能有默认值。
+
+// 中缀函数调用的优先级低于算术操作符、类型转换以及 rangeTo 操作符。
+// 1 shl 2 + 3 等价于 1 shl (2 + 3)
+// 0 until n * 2 等价于 0 until (n * 2)
+// xs union ys as Set<*> 等价于 xs union (ys as Set<*>)
+
+// 中缀函数调用的优先级高于布尔操作符 && 与 ||、is- 与 in- 检测以及其他一些操作符。
+// a && b xor c 等价于 a && (b xor c)
+// a xor b in c 等价于 (a xor b) in c
+infix fun Int.sh(x: Int): Int {
+    return this + x //  this 指的是 Int.sh 中 Int
+}
+
+// 中缀函数总是要求指定接收者与参数。当使用中缀表示法在当前接收者上调用方法时，需要显式使用 this。
+class MyStringCollection {
+    private infix fun add(str: String) {
+
+    }
+
+    fun build() {
+        this add "abc"  // ok
+        add("abc")      // ok
+        // add "abc"    // 错误：必须指定接收者
+    }
+}
