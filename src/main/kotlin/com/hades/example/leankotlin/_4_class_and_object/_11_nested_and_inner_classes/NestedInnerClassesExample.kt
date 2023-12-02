cd96ccbde1f1ffcd3d60ba24ee5af18d3cb0338c
yingvickycao <ying.vicky.cao@outlook.com>
2023-12-02 17:29:13 +0800

    Create NestedInnerClassesExample.kt

---
+++ src/main/kotlin/com/hades/example/leankotlin/_4_class_and_object/_11_nested_and_inner_classes/NestedInnerClassesExample.kt
@@ -0,0 +1,18 @@
+package com.hades.example.leankotlin._4_class_and_object._11_nested_and_inner_classes
+
+// Nested and inner classes (嵌套类与内部类)
+// https://kotlinlang.org/docs/nested-classes.html
+// https://book.kotlincn.net/text/nested-classes.html
+fun main() {
+    /**
+     * Example 1 : Nested and inner classes
+     */
+
+    /**
+     * Example 1 : Inner classes
+     */
+
+    /**
+     * Example 1 : Anonymous inner classes
+     */
+}
