e82a3d4b3c88f4b3cbeda71f2876a68d172c96d9
yingvickycao <ying.vicky.cao@outlook.com>
2023-12-10 18:47:20 +0800

    Create InlineFuncExample.kt

---
+++ src/main/kotlin/com/hades/example/leankotlin/_5_functions/_3_inline_functions/InlineFuncExample.kt
@@ -0,0 +1,30 @@
+package com.hades.example.leankotlin._5_functions._3_inline_functions
+
+// 内联函数
+// https://book.kotlincn.net/text/inline-functions.html
+// https://kotlinlang.org/docs/inline-functions.html
+
+/**
+ * Example 1 : Inline functions
+ */
+
+/**
+ * Example 2 : noinline
+ */
+
+/**
+ * Example 3 : Non-local returns
+ */
+
+
+/**
+ * Example 4 : Reified type parameters
+ */
+
+/**
+ * Example 5 : Inline properties
+ */
+
+/**
+ * Example 6 : Restrictions for public API inline functions
+ */
