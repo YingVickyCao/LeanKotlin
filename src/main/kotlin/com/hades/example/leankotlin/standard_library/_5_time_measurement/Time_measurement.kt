b8fdf26f7f688b06fbef5339f0f19e9db4b9b931
YingVickyCao <yingvickycao@163.com>
2024-10-31 08:41:28 +0800

    Update Time_measurement.kt

--- src/main/kotlin/com/hades/example/leankotlin/standard_library/_5_time_measurement/Time_measurement.kt
+++ src/main/kotlin/com/hades/example/leankotlin/standard_library/_5_time_measurement/Time_measurement.kt
@@ -4,16 +4,21 @@ import com.hades.example.leankotlin.concepts._1_types._2_data_type._1_basic_type
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.microseconds
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.nanoseconds
 import kotlin.time.Duration.Companion.seconds
+import kotlin.time.DurationUnit
 
 // https://kotlinlang.org/docs/time-measurement.html
 // Time measurement
 fun main() {
-    test1_1()
-    test1_2()
+//    test1_1()
+//    test1_2()
+//    test1_3()
+//    test1_4()
+    test1_5()
 }
 /**
  * Calculate duration
@@ -67,18 +72,66 @@ private fun test1_1() {
  * Calculate duration - Get string representation
  */
 private fun test1_2() {
-    
+    // configure the output
+
+    fun test1_2_example1() {
+        // Print in seconds with 2 decimal places
+        println(5887.milliseconds.toString(DurationUnit.SECONDS, 2)) // 5.89s
+    }
+
+    fun test1_2_example2() {
+        // Get an ISO-8601-compatible string
+        println(86420.seconds.toIsoString()) // PT24H0M20S
+        println(60.seconds.toIsoString()) // PT1M
+    }
+    test1_2_example1()
+    test1_2_example2()
 }
 
 /**
  * Calculate duration - Convert duration
  */
+private fun test1_3() {
+    // convert Duration into diferent DurationUnit
+    fun test1_3_example1() {
+        val thirtyMinutes: Duration = 30.minutes
+        println(thirtyMinutes.inWholeSeconds) // 1800
+    }
+
+    fun test1_3_example2() {
+        println(270.seconds.toDouble(DurationUnit.MINUTES)) // 4.5
+        println(60.seconds.toDouble(DurationUnit.MINUTES)) // 1.0
+        println(61.seconds.toDouble(DurationUnit.MINUTES)) // 1.0166666666666666
+    }
+    test1_3_example1()
+    test1_3_example2()
+}
+
 /**
  * Calculate duration - Compare duration
  */
+private fun test1_4() {
+    fun test1_4_example1() {
+        // To check if Duration object are equal
+        val thirtyMinutes: Duration = 30.minutes
+        val halfHour: Duration = 0.5.hours
+        println(thirtyMinutes == halfHour) // true
+    }
+
+    fun test1_4_example2() {
+        // To compare Duration objects
+        println(3000.microseconds < 25000.nanoseconds) // false
+    }
+    test1_4_example1()
+    test1_4_example2()
+}
+
 /**
  * Calculate duration - Break duration into components
  */
+private fun test1_5() {
+
+}
 
 /**
  * Measure time
