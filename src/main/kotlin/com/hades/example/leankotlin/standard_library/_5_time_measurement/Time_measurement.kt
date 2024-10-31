295af372cecea1ff064ee94da3229367c97d1285
YingVickyCao <yingvickycao@163.com>
2024-10-31 09:47:28 +0800

    Update Time_measurement.kt

--- src/main/kotlin/com/hades/example/leankotlin/standard_library/_5_time_measurement/Time_measurement.kt
+++ src/main/kotlin/com/hades/example/leankotlin/standard_library/_5_time_measurement/Time_measurement.kt
@@ -1,7 +1,7 @@
 package com.hades.example.leankotlin.standard_library._5_time_measurement
 
 import com.hades.example.leankotlin.concepts._1_types._2_data_type._1_basic_types._1_5_file_name._custom_class_name.f
-import kotlin.time.Duration
+import kotlin.time.*
 import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.microseconds
@@ -9,7 +9,6 @@ import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.nanoseconds
 import kotlin.time.Duration.Companion.seconds
-import kotlin.time.DurationUnit
 
 // https://kotlinlang.org/docs/time-measurement.html
 // Time measurement
@@ -18,7 +17,10 @@ fun main() {
 //    test1_2()
 //    test1_3()
 //    test1_4()
-    test1_5()
+//    test1_5()
+//    test2_1()
+//    test2_2()
+    test2_3()
 }
 /**
  * Calculate duration
@@ -130,7 +132,8 @@ private fun test1_4() {
  * Calculate duration - Break duration into components
  */
 private fun test1_5() {
-
+    val thirtyMinutes: Duration = 30.minutes
+    println(thirtyMinutes.toComponents { hours: Long, minutes: Int, seconds: Int, nanoseconds: Int -> "${hours}h:${minutes}m:${seconds}s:${nanoseconds}ns" }) // 0h:30m:0s:0ns
 }
 
 /**
@@ -140,13 +143,72 @@ private fun test1_5() {
 /**
  * Measure time - Measure code execution time
  */
+private fun test2_1() {
+    fun test2_1_example1() {
+        // To measure the time taken to execute a block of code,use the measureTime inline function
+        val timeTaken = measureTime {
+            Thread.sleep(100)
+        }
+        println(timeTaken) // 105.258792ms
+    }
+
+    fun test2_1_example2() {
+        // To measure the time taken to execute a block of code and return the value of the block of code, use inline function measureTimedValue.
+        val (value, timeTaken) = measureTimedValue {
+            Thread.sleep(100)
+            42
+        }
+        println(value) // 42
+        println(timeTaken) // 102.910125ms
+    }
+    // By default, both functions use a monotonic time source.
+    test2_1_example1()
+    test2_1_example2()
+}
+
 /**
- * Measure time - Mark moments in time
+ * TODO:Measure time - Mark moments in time
  */
+private fun test2_2() {
+    // To mark a specific moment in time, use the TimeSource interface and the markNow() function to create a TimeMark:
+    val timeSource = TimeSource.Monotonic
+    val mark = timeSource.markNow()
+    println(mark) // ValueTimeMark(reading=1333)
+}
 
 /**
  * Measure time - Measure differences in time
  */
+private fun test2_3() {
+    fun test2_3_example1() {
+        // To measure differences between TimeMark objects from the same time source, use the subtraction operator (-).
+        // To compare TimeMark objects from the same time source, use the comparison operators (<, >).
+        val timeSource = TimeSource.Monotonic
+        val mark1 = timeSource.markNow()
+        Thread.sleep(500)
+        val mark2 = timeSource.markNow()
+        repeat(4) { n ->
+            val mark3 = timeSource.markNow()
+            val elapsed1 = mark3 - mark2
+            val elapsed2 = mark2 - mark1
+            println("Measurement 1.${n + 1}:elapsed1=$elapsed1,elapsed2=$elapsed2,diff=${elapsed2 - elapsed1}")
+        }
+        println(mark2 > mark1)
+
+        // log:
+        // Measurement 1.1:elapsed1=6.667us,elapsed2=503.958292ms,diff=503.951625ms
+        // Measurement 1.2:elapsed1=35.976542ms,elapsed2=503.958292ms,diff=467.981750ms
+        // Measurement 1.3:elapsed1=36.031417ms,elapsed2=503.958292ms,diff=467.926875ms
+        // Measurement 1.4:elapsed1=36.077292ms,elapsed2=503.958292ms,diff=467.881ms
+        // true
+    }
+
+    fun test2_3_example2() {
+
+    }
+    test2_3_example1()
+    test2_3_example2()
+}
 
 /**
  * Time sources
@@ -154,6 +216,13 @@ private fun test1_5() {
 /**
  * Time sources - Default time sources per platform
  */
+private fun test3_1() {
+
+}
+
 /**
  * Time sources - Create time source
  */
+private fun test3_2() {
+
+}
